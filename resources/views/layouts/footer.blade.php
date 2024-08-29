2ce515eaeb56b52127733f5a0d952db063936d76
farooqhalimzaigithub <farooqhalimzaigithub24@gmail.com>
2024-08-29 13:44:52 -0700

    create new laravel 10 project and also set dashboard

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        <a href="https://payplus.pk">Payplus.pk</a></a>
+    </div>
+    <div class="footer-right">
+    </div>
+</footer>
